21a2ccd1a2d8b4f04f152da23dc92dedd773cac3
Dean Merchant <dean.merchant@softwire.com>
2016-01-15 16:23:23 +0000

    beep boop

--- index.js
+++ index.js
@@ -18,51 +18,49 @@ const turnOffAllLights = () => io.writePins([{ pinId: greenPinId, value: false }
 const onBuildSuccess = () => turnOffAllLights().then(() => io.writePin(greenPinId, true));
 const onBuildFailure = () => turnOffAllLights().then(() => io.writePin(redPinId, true));
 
-app.get('/status', (request, res) => {
-  res.send('ok');
-});
+app.get('/status', (request, response) => response.send('beep boop'));
 
-app.post('/build', (request, res) => {
+app.post('/build', (request, response) => {
   const { name, build: { phase, status } } = request.body;
-  logger.info("post recieved:", name, phase, status);
+  logger.info(`Build notification received name: ${name}, phase: ${phase}, status: ${status}`);
   const changeBuildStatus = (status === 'STABLE') ? onBuildSuccess() : onBuildFailure();
 
   changeBuildStatus
-    .then(res.status(200).send)
+    .then(() => response.status(200).send())
     .catch((err) => {
       logger.error(err);
-      res.status(500).send(err);
+      response.status(500).send(err);
     });
 });
 
-app.post('/light/:color/:on', (req, res) => {
-  const { color, on } = req.params;
+app.post('/light/:color/:on', (request, response) => {
+  const { color, on } = request.params;
   logger.info('Manual color set', color, on);
 
   const isValidUrl = (contains(['green', 'red'], color) && contains(['on', 'off'], on));
   if ( ! isValidUrl) {
     const errorMessage = `Invalid url, sent color: ${color} on/off: ${on}`;
     logger.error(errorMessage);
-    return res.status(400).send({ error: errorMessage });
+    return response.status(400).send({ error: errorMessage });
   }
 
   const pinId = color == 'green' ? greenPinId : redPinId;
   const value = on === 'on';
 
   io.writePin(pinId, value)
-    .then(() => { logger.info(`Changed light status${color}: ${on}`); })
-    .then(res.status(200).send)
+    .then(() => logger.info(`Changed light status${color}: ${on}`))
+    .then(() => response.status(200).send())
     .catch((err) => {
       logger.error(`Failed to change light status for: ${color} to: ${on}`);
-      res.status(500).send({ error: err.message })
+      return response.status(500).send({ error: err.message })
     });
 });
 
 setupPins.then(() => {
   const port = 9000;
   app.listen(port, (err) => {
-    if (err) {throw err; }
-    logger.info(`listening on ${port}`);
-    logger.info(`Is running on raspberryPi: ${process.env.IsPI}`);
+    if (err) { throw err; }
+    logger.info(`Listening on ${port}`);
+    logger.info(`Running on raspberryPi: ${process.env.IsPI}`);
   });
 }).catch(logger.error);
